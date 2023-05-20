4da66da17c8af7438ec05a1c351a442db191d3dc
Krystian Derdzinski <krystian.derdzinski@columbusglobal.com>
2023-05-20 10:36:31 +0200

    API key, get playlist items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { API_KEY } from './API_KEY.const';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'streaming-service';
+
+  ngOnInit(): void {
+    this.getPlaylistItems();
+  }
+
+  getPlaylistItems() {
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PL8B03F998924DA45B&key=${API_KEY}`
+    )
+      .then((data) => data.json())
+      .then((data) => console.log(data));
+  }
 }
